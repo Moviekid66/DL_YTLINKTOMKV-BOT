e839b95328d93d9f2cc91c2cdc87bd17cc0085f4
Moviekid66 <110978144+Moviekid66@users.noreply.github.com>
2022-08-18 01:13:56 +0530

    Update love.py

--- workers/love.py
+++ workers/love.py
@@ -16,22 +16,6 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def love(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("♂️ Github", url="https://github.com/PredatorHackerzZ")],
+        [InlineKeyboardButton("♂️ Github", url="https://github.com/DL_YTLINKTOMKV")],
     ])
     Aww = f"""Hey <b>{message.from_user.first_name}</b>
-If you liked my project and want to be a GitHub contributor then:
-- 🗣️ You may contact at **@TeleRoid14**
-
-- 🧾 You can personal message me in Telegram **@HelpLessBoi**   
-
-- Star & Fork my GitHub Repo else You are Gay.\n
-
-If you liked my project and want and want to Donate then you can:
-- [PayPal](https://paypal.me/AbhishekKumarIN47) 
-    
-**<b>{message.from_user.first_name}</b> 😁 Thanks a lot for using my bot**
-
-[@YoutubeXDBot](https://telegra.ph/file/a532f298b920e99bd58bb.jpg)
-"""      
-    await message.reply_text(Aww, reply_markup=joinButton)
-    raise StopPropagation
