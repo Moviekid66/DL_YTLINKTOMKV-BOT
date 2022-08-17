a04ab9c9993b8c082aa1094e88c372faf5a43c19
Moviekid66 <110978144+Moviekid66@users.noreply.github.com>
2022-08-18 01:17:36 +0530

    Update youtubelink.py

--- workers/youtubelink.py
+++ workers/youtubelink.py
@@ -16,11 +16,11 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def love(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("⭕ @TellyFun ⭕", url="https://t.me/+VLVXL_BjK5dge1iW")],
-        [InlineKeyboardButton("⭕ @HindiWebNetwork ⭕", url="https://t.me/+ffu_vfyIiU01NTZl")],
-        [InlineKeyboardButton("⭕ BotsList ⭕", url="https://t.me/joinchat/t1ko_FOJxhFiOThl")],
-        [InlineKeyboardButton("⭕ Leech & Mirror ⭕", url="https://t.me/+N9Pi9xjYX6E3YmE9")],
-        [InlineKeyboardButton("⭕ Movies & Demand ⭕", url="https://t.me/+jD86CLZqqUBjZDk1")]
+        [InlineKeyboardButton("⭕ Dldupdates⭕", url="https://t.me/dldupdates")],
+        [InlineKeyboardButton("⭕ WebNetwork ⭕", url="https://t.me/dldupdates")],
+        [InlineKeyboardButton("⭕ BotsList ⭕", url="https://t.me/dldupdates")],
+        [InlineKeyboardButton("⭕ Leech & Mirror ⭕", url="https://t.me/dldupdates")],
+        [InlineKeyboardButton("⭕ Movies & Demand ⭕", url="https://t.me/dldupdates")]
     ])
     youtube_ex = f"""
 **Some example youtube channels and songs if you don't know then what u want**
